0a793086fc563b044421bf4f2574f479dccadb8a
Welington Alves <alveswelington610@gmail.com>
2024-05-31 22:03:40 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once("./helpers/url.php");
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>Ola, Mundo</h1>
+
+</body>
+
+</html>
